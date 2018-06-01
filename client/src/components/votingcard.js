e682ad05fe3b04ecf9c0480256702bdc288639fc
cmoore4os <cm4glos@gmail.com>
2018-06-01 13:41:05 -0400

    edit -- Change voting to tally votes

--- client/src/components/votingcard.js
+++ client/src/components/votingcard.js
@@ -21,18 +21,73 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 class VotingCard extends Component {
-  state = { data: [],activeUser:[] }
+  state = { data: [], activeUser: [], activeCharities:[] }
+  SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3030';
   
+  componentDidMount() {
+  // get the current user
+    this.getActiveUser();
+    this.getActiveCharities();
+    
+  // get the activeCharities
+  }
 
+  getActiveUser = () => {
+      const currentUser = sessionStorage.getItem('user') || '5b1028f62cd2dc1e51bdf952'
+    console.log(sessionStorage.getItem('user'))
+    axios
+      .get(`${this.SERVER_URL}/users/${currentUser}`)
+      .then(response => {
+        this.setState({
+          activeUser: response.data,
+        });
+        console.log(response.data)
+      })
+      .catch(() => {
+        console.error('error getting data');
+      });
+  }
+
+  getActiveCharities = () => {
+    const active =  []
+    axios
+      .get(`${this.SERVER_URL}/charities`)
+      .then(response => {
+        response.data.forEach((e) => {
+          if (e.active==true) active.push(e)
+        })
+        this.setState({
+          activeCharities: active,
+        });
+        console.log(response.data)
+      })
+      .catch(() => {
+        console.error('error getting data');
+      });
+    
+
+  }
 
  handleChange = (event) => {
   //const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 	// the select button will increment the tally counter for the appropriate charity
 	// the amount the tally counter will be incremented by depends on the user's subscription rate
   // for now, the select as winner page will just be a route to the thank you page
+   
+    let pledgeAmount = 10
+   if (this.state.activeUser && this.state.activeUser.userPledge) pledgeAmount = this.state.activeUser.userPledge;
+   const votes = pledgeAmount / 10
 
-  
-}
+   axios
+      .put(`${this.SERVER_URL}/charities/id`,votes)
+      .then(response => {
+        console.log('response from post', response);
+      })
+      .catch(() => {
+        console.error('error getting data');
+      });
+  }
+
   render() {
     return (
       <div className="VotingCard">
