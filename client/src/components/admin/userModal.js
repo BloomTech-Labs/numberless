b9eab77b2aba5be02376937c8b35ad153eebc051
Justin Borek <justinborek@gmail.com>
2018-06-06 18:13:19 -0500

    continued work on user modal, renders correctly, now just have to create user on click

--- client/src/components/admin/userModal.js
+++ client/src/components/admin/userModal.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import { 
+  Button, 
+  Modal,
+  ModalHeader, 
+  ModalBody, 
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input
+} from 'reactstrap'
 
 import './modal.css'
 
 const UserModal = props => {
   const toggle = props.toggle;
   const modal = props.modal;
+  const addUser = () => {
+    console.log('adding!');
+  }
   return (
     <div>
         <Button color="danger" onClick={toggle}>Add User</Button>
         <Modal className="modalBox" isOpen={modal} toggle={toggle}>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <ModalBody className="bodyBox">
+            <div className="modalForm">
+              <Form>
+                <FormGroup>
+                  <Input className="input" type="email" name="email" id="email" placeholder="Email"/>
+                </FormGroup>
+                <FormGroup>
+                  <Input className="input" type="password" name="password" id="password" placeholder="Password"/>
+                </FormGroup>
+              </Form>
+            </div>
           </ModalBody>
           <ModalFooter className="footer">
-            <Button className="modalButton" color="primary" onClick={toggle}>Do Something</Button>{' '}
+            <Button className="modalButton" color="primary" onClick={ () => {
+              toggle();
+              addUser();
+            }}>Submit</Button>{' '}
             <Button className="modalButton" color="secondary" onClick={toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
